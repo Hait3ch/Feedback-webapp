fa64fc6eb4c1d77a94c08db5bddbb9d703b4c293
Hai Phan <hatazhix@gmail.com>
2021-02-20 09:56:39 +0200

    nodemon setup

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    cliendID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret 
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken)
+        console.log(profile)
+    }
+)
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
